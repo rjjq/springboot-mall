4dcca0663ca0a4a0d7a62b99929414338698f186
rjjq <rrrjjjqqq@gmail.com>
2024-08-02 23:02:33 +0800

    Product - updateProduct

--- src/main/java/com/rjjq/springbootmall/dao/impl/ProductDaoImpl.java
+++ src/main/java/com/rjjq/springbootmall/dao/impl/ProductDaoImpl.java
@@ -61,4 +61,22 @@ public class ProductDaoImpl implements ProductDao {
         Integer productId = keyHolder.getKey().intValue();
         return productId;
     }
+
+    @Override
+    public void updateProduct(Integer productId, ProductRequest productRequest) {
+        String sql = "UPDATE product SET product_name = :product_name, category = :category, image_url = :image_url, price = :price, stock = :stock, description = :description, last_modified_date = :last_modified_date WHERE product_id = :product_id;" ;
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("product_id", productId);
+        map.put("product_name", productRequest.getProductName());
+        map.put("category", productRequest.getCategory().toString());
+        map.put("image_url", productRequest.getImageUrl());
+        map.put("price", productRequest.getPrice());
+        map.put("stock", productRequest.getStock());
+        map.put("description", productRequest.getDescription());
+        map.put("last_modified_date", new Date());
+
+        namedParameterJdbcTemplate.update(sql, map);
+    }
+
 }
