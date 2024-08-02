58066a9da9ec176dc4d41b3da52e25b1954e3bcf
rjjq <rrrjjjqqq@gmail.com>
2024-08-02 22:37:09 +0800

    Product - createProduct

--- src/main/java/com/rjjq/springbootmall/dao/ProductDao.java
+++ src/main/java/com/rjjq/springbootmall/dao/ProductDao.java
@@ -1,7 +1,10 @@
 package com.rjjq.springbootmall.dao;
 
+import com.rjjq.springbootmall.dto.ProductRequest;
 import com.rjjq.springbootmall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/rjjq/springbootmall/service/ProductService.java
+++ src/main/java/com/rjjq/springbootmall/service/ProductService.java
@@ -1,7 +1,10 @@
 package com.rjjq.springbootmall.service;
 
+import com.rjjq.springbootmall.dto.ProductRequest;
 import com.rjjq.springbootmall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
